032f3fe90dce5afdbf8b9ed5678109163404ae7f
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 10:28:54 -0300

    Dev commit 25
    criadas as variáveis de PontuacaoUIController

--- Assets/Scripts/PontuacaoUIController.cs
+++ Assets/Scripts/PontuacaoUIController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PontuacaoUIController : MonoBehaviour
 {
+    //Referência da UI de texto de pontuação
+    public Text textPontuacao;
+
+    //Número do "Actor" do Photon PUN para ter a referência de quem pertence esta UI
+    public int actorNumber;
+
     // Start is called before the first frame update
     void Start()
     {
