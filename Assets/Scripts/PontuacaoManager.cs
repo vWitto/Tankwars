0e72f81b6386d43b909220aa494d8b4ca7aa3ff9
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 10:26:08 -0300

    Dev commit 23
    feito o método AdicionarPontuacao

--- Assets/Scripts/PontuacaoManager.cs
+++ Assets/Scripts/PontuacaoManager.cs
@@ -1,13 +1,27 @@
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PontuacaoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Adiciona a pontuação ao jogador
+    public void AdicionarPontuacao(Player player)
     {
-        
+        //Inicializa a pontuação como zero e obtem a pontuação do jogador
+        int pontuacaoAtual = 0;
+        if (player.CustomProperties.ContainsKey("Pontuacao"))
+        {
+            pontuacaoAtual = (int)player.CustomProperties["Pontuacao"];
+        }
+
+        //Adiciona a pontuação em 1
+        pontuacaoAtual += 1;
+
+        //Atualiza a pontuação no PhotonPun e notifica todos jogadores, fazendo isso, o Photon Pun executará o método OnPlayerPropertiesUpdate da classe PontuacaoUIController
+        Hashtable propriedadePontuacao = new Hashtable();
+        propriedadePontuacao["Pontuacao"] = pontuacaoAtual;
+        player.SetCustomProperties(propriedadePontuacao);
     }
 
     // Update is called once per frame
