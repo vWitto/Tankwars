a6a0256e2ac50d8eb5988aaa4a8ec5b7d51437f2
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 10:06:02 -0300

    Dev commit 12
    feito o método Update em CanhaoStats

--- Assets/Scripts/TanqueCanhaoStats.cs
+++ Assets/Scripts/TanqueCanhaoStats.cs
@@ -12,15 +12,31 @@ public class TanqueCanhaoStats : MonoBehaviour, IShootable
     public float _frequenciaBala = 1f;
     public float _frequenciaBalaAtual = 0f;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Atirar()
     {
-        
+        // Reseta a frequência para 0, pois o tanque precisa recarregar a bala de canhão para o próximo tiro
+        _frequenciaBalaAtual = 0f;
+
+        //Dispara a bala e instância entre a rede
+        var bala = PhotonNetwork.Instantiate("BalaPrefab", LocalizacaoSaidaBala.transform.position, LocalizacaoSaidaBala.transform.rotation);
+        bala.GetComponent<BalaController>().Inicializar(GetComponentInParent<TanqueController>().gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Contabiliza a frequência da bala, pois o tanque está carregando a bala de canhão
+        _frequenciaBalaAtual += Time.deltaTime;
+
+        //Verifica se "sou eu" atirando
+        if (photonView.IsMine)
+        {
+            //Verifica se o botão de espaço do teclado foi apertado e se o tanque já está carregado para atirar
+            if (Input.GetKeyDown(KeyCode.Space) && _frequenciaBalaAtual > _frequenciaBala)
+            {
+                //Executa o método de atirar a bala de canhão
+                Atirar();
+            }
+        }
     }
 }
