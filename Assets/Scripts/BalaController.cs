4f9ce9d654e2164cf002c5bc6699bf1dc99b7ae7
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 09:57:11 -0300

    Dev commit 8
    feito o método de colisão com a bala

--- Assets/Scripts/BalaController.cs
+++ Assets/Scripts/BalaController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,5 +25,32 @@ public class BalaController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Verifica se a bala está colidindo com o atirador, neste caso, não deve fazer nada
+        if (atirador == collision.gameObject)
+        {
+            Debug.Log("A bala passou pelo próprio atirador");
+            return;
+        }
+
+        //Obtem e verifica se o objeto com quem a bala colidiu, pode receber dano
+        IDamageable damageable = collision.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            //Faz com que o objeto que recebeu o tiro e pode receber dano, receba o dano
+            damageable.ReceberDano();
+
+            //Verifica se a bala "é minha", se for e acertou o alvo, contabiliza um ponto
+            if (photonView.IsMine)
+            {
+                //Adiciona a pontuação para o jogador atual
+                FindObjectOfType<PontuacaoManager>().AdicionarPontuacao(PhotonNetwork.LocalPlayer);
+            }
+        }
+
+        AutoDestruir();
     }
 }
