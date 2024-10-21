423a56f91179371d00d24f7eb64fe055f6dbebda
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 10:31:12 -0300

    Dev commit 27
    criadas as variáveis de GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,24 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
+    //Referência as localizações que cada jogador começa
+    public List<GameObject> localizacoesSpawn;
+
+    //Referência ao texto de UI que tem o cronômetro
+    public Text textTimer;
+
+    //Tempo de partida em segundos
+    public float tempoDePartida = 120f;
+    private float tempoDePartidaAtual = 0f;
+
+    //Boolean informando se o jogo finalizou
+    public bool ehGameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
