a9faf6b256555735491e338937ba63dbb4ed5d3d
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 10:03:34 -0300

    Dev commit 11
    criadas as variáveis de CanhaoStats

--- Assets/Scripts/TanqueCanhaoStats.cs
+++ Assets/Scripts/TanqueCanhaoStats.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class TanqueCanhaoStats : MonoBehaviour
+public class TanqueCanhaoStats : MonoBehaviour, IShootable
 {
+    // Localização de onde vai sair a bala
+    public Transform LocalizacaoSaidaBala;
+
+    // Frequência que o tanque pode atirar uma bala de canhão, é o tempo que ele leva para carregar.
+    public float _frequenciaBala = 1f;
+    public float _frequenciaBalaAtual = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
