bcfc720ebb9b7bbb1364564a77f4486ca4d2a363
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 09:55:45 -0300

    Dev commit 7
    criadas as variáveis de BalaController

--- Assets/Scripts/BalaController.cs
+++ Assets/Scripts/BalaController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class BalaController : MonoBehaviour
 {
+    //Velocidade da bala
+    public float velocidade = 5f;
+
+    //Tempo de vida até a bala se auto destruir
+    public float tempoDeVida = 3f;
+    float tempoDeVidaAtual = 0f;
+
+    //GameObject do tanque que disparou
+    private GameObject atirador;
+
     // Start is called before the first frame update
     void Start()
     {
