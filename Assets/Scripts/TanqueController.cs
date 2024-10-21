ae97b2f73a3c07afc7ac594031b5f6ded98f2392
vWitto <161627832+vWitto@users.noreply.github.com>
2024-10-21 09:00:01 -0300

    dev commit 1
    criadas as variáveis no TanqueController

--- Assets/Scripts/TanqueController.cs
+++ Assets/Scripts/TanqueController.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TanqueController : MonoBehaviour
 {
+
+    //Velocidade de rotação do tanque
+    public float _velocidadeRotacao = 100f;
+
+    //Velocidade de movimento do tanque
+    public float _velocidadeMovimento = 5f;
+
+    private Rigidbody2D rb;
+    private GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
